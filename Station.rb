2dcc531a5f30530cdc265152aa6ca37404ac9cd4
Sbhboi <142192418+Sbhboi@users.noreply.github.com>
2023-08-22 15:29:09 +0800

    first commit station file

---
+++ Station.rb
@@ -0,0 +1,18 @@
+class Station
+    def initialize(place, fare)
+      @place = place 
+      @fare = fare
+    end
+  
+    def place
+      @place 
+    end
+  
+    def distance
+      @distance 
+    end
+  
+    def fare
+      @fare
+    end
+end
