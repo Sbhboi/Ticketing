8e3ecdce866a9c27c198aa367232066609b5a7a5
Sbhboi <142192418+Sbhboi@users.noreply.github.com>
2023-08-22 16:43:18 +0800

    2nd commit

--- Card.rb
+++ Card.rb
@@ -14,12 +14,4 @@ class Card
     def deduct_fare(amount)
       @deduct_fare
     end
-
-    def student_rate # to give 15% discounts on fare
-        @student_rate
-    end
-
-    def elder_rate # to give 50% discount  on fare
-        @elder_rate
-    end
 end
