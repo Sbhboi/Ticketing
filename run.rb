d3fc21c9c6aee6a2f60dcdc66299682da418f51a
Sbhboi <142192418+Sbhboi@users.noreply.github.com>
2023-08-24 14:39:19 +0800

    3rd commit

--- run.rb
+++ run.rb
@@ -1,33 +1,56 @@
 require './Card.rb'
 require "./Rider.rb"
 require "./Station.rb"
+require "./Cardtype.rb"
 
   card = Card.new(0)
-  rider1 = Rider.new("Eva", "0168170407", "101")
-  rider2 = Rider.new("Pame", "0165869373", "102")
+  # rider1 = Rider.new("Angkol", "101", "Senior Card")
+  # rider2 = Rider.new("BuiBui", "102", "Student Card")
 
   puts "DBKK transport System"
-  puts "Name: #{rider1.name}"
-  # puts "Card No: #{rider1.card}"
-  puts "Card balance is $: #{rider1.card.balance}" # 0
-  puts "Your top up amount $: #{rider1.card.top_up_card(10)}"
-  puts "Your card balance now is $: #{rider1.card.balance}" #10
-   
-  station1 = Station.new("Riverson", 2)
-  station2 = Station.new("One Borneo", 5)
-  station3 = Station.new("Papar", 69)
-  station4 = Station.new("Putatan-Petagas", 18)
+  puts "enter your name"
+  name = gets.chomp
+  puts "Good day #{name}"
+  puts "Your card's current balance is $: #{card.balance}"
+  puts "Enter your top up amount"
+  top_up_card = gets.chomp
+  puts "Successfully top up amount of $: #{top_up_card}"
+  puts "Your card's current balance is $: #{top_up_card}"
   
-  puts "Destination: 1: #{station1.place}, 2: #{station2.place}, 3: #{station3.place}, 4: #{station4.place}"
-  puts "Select the destination"
 
-# 1 = station1
+#  puts "Name: #{rider1.name}"
+  # puts "Card No: #{name.card_number}"
+  # puts "Card type is: #{rider1.card}"
+  # puts "Card balance is $: #{name.card.balance}" # 0
+  # puts "Your top up amount $: #{name.card.top_up_card(10)}"
+  # puts "Your card balance now is $: #{name.card.balance}" #10
+   
+  place1 = Station.new("Riverson", 5)
+  place2 = Station.new("One Borneo", 12)
+  place3 = Station.new("Papar", 69)
+  place4 = Station.new("Putatan-Petagas", 18)
+  
+  # 1 == station1
+  # 2 == station2
+  # 3 == station3
+  # 4 == station4
+  
 
-# 2 = station2
-# 3 = station3
-# 4 = station4
+  # puts "1: #{station1.place}"
+  # puts "2: #{station2.place}"
+  # puts "3: #{station3.place}"
+  # puts "4: #{station4.place}"
+  # puts "Select station origin"
+  # origin = gets.chomp.to_s
+  # puts "Select station destination"
+  # destination = gets.chomp.to_s
+  # puts "Proceeding from #{origin} to #{destination}, the fare is #{@fare}"
 
-answer = gets.chomp
+
+
+  puts "Where do you wish to go ?" 
+    puts "1 - Riverson, 2 - One Borneo, 3 - Papar, 4 - Putatan-Petagas"
+    answer = gets.chomp
 if answer == 1
   puts "Traveling to Riverson"
     elsif answer == 2
@@ -37,6 +60,7 @@ if answer == 1
     elsif answer == 4
       puts "Traveling to Putatan-Petagas"
     else
-      puts "proceed for payment"
-
+      puts "proceed for payment, the fare is #{@fare}"
+      # puts "Proceeding from #{origin} to #{destination}, the fare is #{@fare}"
 end
+
