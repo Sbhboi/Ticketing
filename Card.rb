eb0cb0d2d5fdf32134becf4e29c2c4dc56370f54
Sbhboi <142192418+Sbhboi@users.noreply.github.com>
2023-08-24 14:40:05 +0800

    3rd commit

--- Card.rb
+++ Card.rb
@@ -1,17 +1,25 @@
 class Card
+  attr_reader :balance, :deduct_fare
+
+
     def initialize(balance)
       @balance = 0
     end
   
-    def balance
-      @balance
-    end
+    # def balance
+    #   @balance
+    # end
   
     def top_up_card(amount)
       @balance += amount
     end
   
-    def deduct_fare(amount)
-      @deduct_fare
+    # def deduct_fare(amount)
+    #   @deduct_fare
+    # end
+    def travels
+      event = [Configuration::PROMOTIONAL.key.sample.default].sample
+      @fare = (destination.fare(event) - origin.fare(event)).abs
     end
+
 end
