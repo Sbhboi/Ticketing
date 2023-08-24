5532e1d20f8a7a40b13833eeef61929c2abf33c1
Sbhboi <142192418+Sbhboi@users.noreply.github.com>
2023-08-24 14:40:42 +0800

    3rd commit

--- Rider.rb
+++ Rider.rb
@@ -1,24 +1,27 @@
 class Rider
-    def initialize (name, contact_number, card_number)
+  attr_reader :name, :card_number, :card
+
+    def initialize (name, card_number, card_type)
       @name = name
-      @contact_number = contact_number
       @card_number = card_number
+      @card_type = card_type
       @card = Card.new(0)
     end
   
-    def name
-      @name 
-    end
+    # def name
+    #   @name 
+    # end
   
-    def card
-      @card
-    end
-  
+    # def card
+    #   @card
+    # end
+    
     def travels (origin, destination)
       origin = origin
       destination = destination
   
-      fare = destination.travel_fare - origin.travel_fare
+      standard_fare = (destination.travel_fare(event) - origin.travel_fare(event)).abs
+      discounted_fare = (standard_fare - discount).abs
       abs_fare = fare.abs
       card.deduct_fare(abs_fare)
     end
